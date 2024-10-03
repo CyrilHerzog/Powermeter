a3bf76b25e47e01afa8b25d9675b3d9b2dc3e867
Cyril Herzog <34273171+CyrilHerzog@users.noreply.github.com>
2024-10-04 01:14:01 +0200

    Add files via upload

---
+++ Powermeter_uC/Powermeter/Debug/Src/adc.d
@@ -0,0 +1,6 @@
+Src/adc.o: ../Src/adc.c ../Inc/adc.h ../Inc/system.h ../Inc/reg.h \
+ ../Inc/gpio.h
+../Inc/adc.h:
+../Inc/system.h:
+../Inc/reg.h:
+../Inc/gpio.h:
---
+++ Powermeter_uC/Powermeter/Debug/Startup/startup_stm32l051r6tx.d
@@ -0,0 +1,1 @@
+Startup/startup_stm32l051r6tx.o: ../Startup/startup_stm32l051r6tx.s
